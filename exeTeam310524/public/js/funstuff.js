d80ac8b0d5994f8a6282a10b10f6f21f18c1043f
leerlandais <leerlandais@gmail.com>
2024-06-01 10:37:29 +0200

    fixed opening animation

--- exeTeam310524/public/js/funstuff.js
+++ exeTeam310524/public/js/funstuff.js
@@ -3,29 +3,25 @@ $(document).ready(function () {
 $(".introHeadings").css("opacity" ,0); // cacher les textes header
 
 // Et puis les faire apparaitre un après les autres
+
+// corriger car slide 45 (pp) n'est pas bien
 $("#introHeading1").queue(function(next){
         $('#introHeading1').fadeTo(2000,1);
     next();
-});
-$("#introHeading1").queue(function(next){
+}).queue(function(next){
         $("#introHeading2").delay(1000).fadeTo(2000,1);
     next();
-});
-$("#introHeading2").queue(function(next){
+}).queue(function(next){
         $("#introHeading3").delay(1500).fadeTo(1000,1);
     next();
-    });
-$("#introHeading3").queue(function(next){
+}).queue(function(next){
         $("#introHeading4").delay(2000).fadeTo(2000,1);
     next();
-});
-$("#introHeading4").queue(function(next){
+}).queue(function(next){
         $("#introHeading5").delay(4000).fadeTo(2000,1).animate({color : "blue"});
         $("#introHeading3").animate({color : "red"},2500);
     next();
-    });
-
-$("#introHeading5").queue(function(next){
+}).queue(function(next){
         $("#introHeading5").animate({color : "aliceblue"},2500);
         $("#introHeading3").animate({color : "aliceblue"},2500);
     next();
