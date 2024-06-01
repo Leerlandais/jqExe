7067968b8c32db07d4dc824425a1361354d2385d
leerlandais <leerlandais@gmail.com>
2024-06-01 10:50:24 +0200

    added disable to buttons

--- exeTeam310524/public/js/funstuff.js
+++ exeTeam310524/public/js/funstuff.js
@@ -31,18 +31,21 @@ $("#introHeading1").queue(function(next){
 
 // Example 0 :
 $('#activateCodeButton0').click(function(){
+    $(this).prop("disabled", true); // désactiver le bouton
     $textHolder = $("#codeDescSpan0").text(); // d'abord copier le texte (puisque il vient du DB)
     $("#codeDescSpan0").text("Voilà, mon texte a changé"); // puis changer
 });
 // reset 0 :
 $("#resetWindowButton0").click(function(){
     $("#codeDescSpan0").text($textHolder); // réinitialise texte (en Ang/Fr)
+    $("#activateCodeButton0").prop("disabled", false); // et réactiver, bien sur
 });
 
 
 
 // Example 1 :
 $('#activateCodeButton1').click(function(){ // même que précedent avec couleur change
+    $(this).prop("disabled", true);
     $textHolder = $("#codeDescSpan1").text(); 
     $("#codeDescSpan1").css('color', 'yellow'); 
     $("#codeDescSpan1").text("Voilà, ma couleur a changé"); 
@@ -51,11 +54,13 @@ $('#activateCodeButton1').click(function(){ // même que précedent avec couleur
 $("#resetWindowButton1").click(function(){
     $("#codeDescSpan1").text($textHolder); 
     $("#codeDescSpan1").css('color', '#d9e2ef'); 
+    $("#activateCodeButton1").prop("disabled", false);
 });
 
 
 // Example 2 :
 $('#activateCodeButton2').click(function(){ 
+    $(this).prop("disabled", true);
     $textHolder = $("#codeDescSpan2").text(); 
     $("#codeDescSpan2").css("color" , 'red').text("Mon texte et ma couleur ont changé"); 
 });
@@ -63,13 +68,15 @@ $('#activateCodeButton2').click(function(){
 $("#resetWindowButton2").click(function(){
     $("#codeDescSpan2").text($textHolder); 
     $("#codeDescSpan2").css('color', '#d9e2ef'); 
+    $("#activateCodeButton2").prop("disabled", false);
 });
 
 
 // Example 3 :
 $('#activateCodeButton3').click(function(){
+    $(this).prop("disabled", true);
     // while trying to get this to work properly, a thorough search of docs shows a better way to write the queue....
-
+    $textHolder = $("#codeDescSpan3").text(); 
 
     $("#codeDescSpan3").text("D'abord, mon text change").delay(1000).queue(function(next){ // change the text and wait
         $(this).text("Puis ma couleur..."); // then the colour
@@ -84,7 +91,12 @@ $('#activateCodeButton3').click(function(){
         next();
     });
 });
-
+// reset 3 :
+$("#resetWindowButton3").click(function(){
+    $("#activateCodeButton3").prop("disabled", false);
+    $("#codeDescSpan3").text($textHolder); 
+    $("#codeDescSpan3").css('color', '#d9e2ef'); 
+});
 
 
 
